21c0b226a650d8151300a8fcf3785cb2cd874350
Ivar Grimstad <ivar.grimstad@eclipse-foundation.org>
2024-04-03 12:55:33 +0200

    Initial Duke Spring AI

---
+++ duke-spring-ai/src/main/java/ai/duke/dukespringai/DukeWithSpringAiApplication.java
@@ -0,0 +1,13 @@
+package ai.duke.dukespringai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DukeWithSpringAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DukeWithSpringAiApplication.class, args);
+	}
+
+}
---
+++ duke-spring-ai/src/test/java/ai/duke/dukespringai/DukeWithSpringAiApplicationTests.java
@@ -0,0 +1,13 @@
+package ai.duke.dukespringai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DukeWithSpringAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
